bd7f79bd49734bd8485a587c982c2edab7e52547
liza <gorbunova.elizaveta@gmail.com>
2022-01-20 17:25:00 +0200

    add base

---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container class="fill-height">
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row justify="center">
+    <v-col align-self="center"
+    justify="center"
+    cols=2>
+      <v-btn class="mb-4">Create room</v-btn>
+      <v-btn>Join room</v-btn>
+    </v-col>
+    
+  </v-row>
+  
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style scoped>
+</style>
