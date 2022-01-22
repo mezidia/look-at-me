ffd44b09c120f9231dd7bd4b7adac017cbfa09c5
Nikita Vasilinenko <nikitavasilinenko@gmail.com>
2022-01-22 17:25:31 +0200

    added form error on modal window

--- components/AcquaintanceModal.vue
+++ components/AcquaintanceModal.vue
@@ -10,12 +10,20 @@
       <v-card-text>
         <h3 class="pt-3 headline">Write down your nickname:</h3>
         <input
+          autocomplete="off"
           class="mt-3 pa-2"
           type="text"
           name="input"
           placeholder="Type here"
           v-model="inputNickname"
+          @input="showFormError = false"
         >
+        <p
+          v-if="showFormError"
+          class="form-error"
+        >
+          Please enter your nickname
+        </p>
       </v-card-text>
       <v-card-actions class="d-flex justify-end">
         <BasicButton
@@ -38,6 +46,7 @@ const { State: ModalState, Action: ModalAction } = namespace('modal');
 @Component({BasicButton})
 export default class AcquaintanceModal extends Vue {
   inputNickname = null;
+  showFormError = false;
 
   @Action setNickname
   @State nickname
@@ -46,10 +55,17 @@ export default class AcquaintanceModal extends Vue {
   @ModalAction setModal
 
   onSubmitClick() {
-    if (!this.inputNickname) return;
+    if (!this.inputNickname) {
+      this.showFormError = true;
+      return;
+    }
     console.log(this.nickname);
     this.setNickname(this.inputNickname);
     this.setModal(false);
+  }
+
+  mounted() {
+    this.showFormError = false;
   }
 }
 </script>
@@ -58,5 +74,10 @@ export default class AcquaintanceModal extends Vue {
   input:focus, input {
     color: whitesmoke;
     font-size: 20px;
+  }
+
+  .form-error {
+    color: red;
+    position: absolute;
   }
 </style>
