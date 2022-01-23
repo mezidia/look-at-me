ed1b5471be7a3d0bf3e12dce0eb11d56a4661d7a
Nikita Vasilinenko <nikitavasilinenko@gmail.com>
2022-01-23 13:47:51 +0200

    F

--- pages/room/_id.vue
+++ pages/room/_id.vue
@@ -11,12 +11,41 @@
     
     <div :style="{'visibility': pageLoading ? 'hidden' : 'visible'}">
       
-      <v-row justify="start" align="start">
-        <v-row id="users-panel" class="fill-height" justify="center">
-          <UserBlock :id="peerId" name="You" :cameraOn="showVideo" :pageLoading="pageLoading" :image="image" :micClicked="micOn" :width="width" :height="height"/>
-          <UserBlock :id="user.peerId" v-for="user in users" :key="user.peerId" :name="user.name" :pageLoading="pageLoading" :image="image" :cameraOn="user.cameraOn" :micClicked="user.micOn" :width="width" :height="height"/>
+      <v-container justify="start" align="start">
+        <v-row v-if="focusedId" id="selectedPanel" class="fill-height" justify="center">
+          <div
+            @click="unselectUser()"
+          >
+            <UserBlock :id="focusedId" :name="focusedName" :cameraOn="showVideo" :pageLoading="pageLoading" :image="image" :micClicked="micOn" width="600" height="450"/>
+          </div>
         </v-row>
-      </v-row>
+        <v-row id="users-panel" class="fill-height" justify="center">
+          <div
+            @click="selectUser(peerId, 'You')"
+            v-if="focusedId !== peerId"
+          >
+            <UserBlock
+              :id="peerId" name="You" :cameraOn="showVideo" :pageLoading="pageLoading" :image="image" :micClicked="micOn" :width="width" :height="height"/>
+          </div>
+          <div
+            @click="selectUser(user.peerId, user.name)"
+            v-for="user in users"
+            :key="user.peerId"
+            :id="user.peerId"
+            :name="user.name"
+          >
+            <UserBlock
+              v-if="user.peerId !== focusedId"
+              :id="user.peerId"
+              :name="user.name"
+              :pageLoading="pageLoading"
+              :image="image" :cameraOn="user.cameraOn"
+              :micClicked="user.micOn" :width="width"
+              :height="height"
+            />
+          </div>
+        </v-row>
+      </v-container>
       <v-row class="play-icon-row">
         <OnOffIcon class="mx-3" big iconName="mdi-video" :onClick="cameraClick" :clicked="showVideo"/>
         <OnOffIcon class="mx-3" big iconName="mdi-microphone" :onClick="micClick" :clicked="micOn"/>
@@ -122,6 +151,9 @@ export default class RoomPage extends Vue {
   snackbarColor = '#40826d';
   snackbarText = 'Your nickname has been updated';
 
+  focusedName = null;
+  focusedId = null;
+  
   socket = null;
   peers = {};
   clients = [];
@@ -205,6 +237,16 @@ export default class RoomPage extends Vue {
   setNotification() {
     this.snackbar = true;
     setTimeout(() => this.snackbar = false, this.snackbarTimeout);
+  }
+
+  selectUser(peerId, userName) {
+    this.focusedId = peerId;
+    this.focusedName = userName;
+  }
+
+  unselectUser() {
+    this.focusedId = null;
+    this.focusedName = null;
   }
 }
 </script>
